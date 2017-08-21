347d39ab397cbb407de01839493cc5f95e955e52
Anton Shumsky <anton.shumsky@gmail.com>
2017-08-21 15:45:50 +0300

    add mapping of log level value

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const coreFields = ['v', 'level', 'name', 'hostname', 'pid', 'time', 'msg', 'src'];
 
-const defaultEncoding = {
-  level: 'logLevel',
+const defaultFieldMapping = {
+  level: ['logLevel', level => logLevelMapping[level]],
   name: 'application',
   time: '@timestamp',
   msg: 'message'
 };
 
-module.exports = function bunyanEncoder(encoding = defaultEncoding, stream = process.stdout) {
+const logLevelMapping = {
+  '10': 'trace',
+  '20': 'debug',
+  '30': 'info',
+  '40': 'warn',
+  '50': 'error',
+  '60': 'fatal'
+};
+
+module.exports = function bunyanEncoder(coreFieldMapping = defaultFieldMapping, stream = process.stdout) {
   return {
     write: function (rec) {
+      if (typeof(rec) === 'string' || (rec instanceof String)) {
+        throw new Error('Trying to log stringified record. Forgot to define "raw: true"?');
+      }
       const recCopy = JSON.parse(JSON.stringify(rec));
+
       coreFields.forEach(field => {
-        if (field in encoding) {
-          recCopy[encoding[field]] = recCopy[field];
+        if (field in coreFieldMapping) {
+          const mapping = coreFieldMapping[field];
+          if (mapping.constructor === Array) {
+            const [name, mapValue] = mapping;
+            recCopy[name] = mapValue(recCopy[field]);
+          } else {
+            recCopy[mapping] = recCopy[field];
+          }
         }
         delete recCopy[field];
       });
+
       stream.write(JSON.stringify(recCopy));
     }
   };
--- test.js
+++ test.js
@@ -18,25 +18,22 @@ describe('bunyan-encoder', () => {
   it('should write message encoded with default layout to stdout', () => {
     //given
     const encoder = bunyanEncoder();
-    const timestamp = new Date().toISOString();
     //when
-    encoder.write({v: '1', name: 'nice-microservice', level: 30, time: timestamp, msg: 'event happened', sessionId: '123'});
+    encoder.write({v: '1', name: 'nice-microservice', level: 30, time: '2017-08-21T09:37:02.567Z', msg: 'event happened', sessionId: '123'});
     //then
     assert.isTrue(process.stdout.write.calledOnce);
     assert.isString(process.stdout.write.firstCall.args[0]);
-    assert.deepEqual(JSON.parse(process.stdout.write.firstCall.args[0]), {application: 'nice-microservice', logLevel: 30, '@timestamp': timestamp, message: 'event happened', sessionId: '123'});    
+    assert.deepEqual(JSON.parse(process.stdout.write.firstCall.args[0]), {application: 'nice-microservice', logLevel: 'info', '@timestamp': '2017-08-21T09:37:02.567Z', message: 'event happened', sessionId: '123'});
   });
 
   it('should write message encoded with defined layout', () => {
     //given
     const encoder = bunyanEncoder({time: 'customTime', msg: 'customMsg'});
-    const timestamp = new Date().toISOString();
     //when
-    encoder.write({v: '1', time: timestamp, msg: 'event happened', sessionId: '123'});
+    encoder.write({v: '1', time: '2017-08-21T09:37:02.567Z', msg: 'event happened', sessionId: '123'});
     //then
     assert.isTrue(process.stdout.write.calledOnce);
-    assert.isString(process.stdout.write.firstCall.args[0]);
-    assert.deepEqual(JSON.parse(process.stdout.write.firstCall.args[0]), {customTime: timestamp, customMsg: 'event happened', sessionId: '123'});
+    assert.deepEqual(JSON.parse(process.stdout.write.firstCall.args[0]), {customTime: '2017-08-21T09:37:02.567Z', customMsg: 'event happened', sessionId: '123'});
   });
 
   it('should write encoded message to a defined stream', () => {
@@ -47,5 +44,10 @@ describe('bunyan-encoder', () => {
     encoder.write({msg: 'event happened'});
     //then
     assert.isTrue(stream.write.calledOnce);
+    assert.deepEqual(JSON.parse(stream.write.firstCall.args[0]), {message: 'event happened'});
+  });
+
+  it('should throw if log record is stringified', () => {
+    assert.throws(() => bunyanEncoder().write('{"msg": "event happened"}'));
   });
 });
