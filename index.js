c3d18c615be55dea09572073c3b93eac7ef09567
Anton Shumsky <anton.shumsky@gmail.com>
2017-08-21 12:34:53 +0300

    add default encoding; refactor tests

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const coreFields = ['v', 'level', 'name', 'hostname', 'pid', 'time', 'msg', 'src'];
 
-module.exports = function bunyanEncoder(encoding, stream = process.stdout) {
+const defaultEncoding = {
+  level: 'logLevel',
+  name: 'application',
+  time: '@timestamp',
+  msg: 'message'
+};
+
+module.exports = function bunyanEncoder(encoding = defaultEncoding, stream = process.stdout) {
   return {
     write: function (rec) {
       const recCopy = JSON.parse(JSON.stringify(rec));
--- test.js
+++ test.js
@@ -4,37 +4,48 @@ const {assert} = require('chai');
 
 describe('bunyan-encoder', () => {
 
-  const stream = {write: () => {}};
   let sandbox;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
-    sandbox.spy(stream, 'write');
+    sandbox.spy(process.stdout, 'write');
   });
 
   afterEach(() => {
     sandbox.restore();
   });
 
-  it('should write encoded message to given stream', () => {
+  it('should write message encoded with default layout to stdout', () => {
     //given
-    const encoder = bunyanEncoder({time: '@timestamp', msg: 'message'}, stream);
+    const encoder = bunyanEncoder();
+    const timestamp = new Date().toISOString();
+    //when
+    encoder.write({v: '1', name: 'nice-microservice', level: 30, time: timestamp, msg: 'event happened', sessionId: '123'});
+    //then
+    assert.isTrue(process.stdout.write.calledOnce);
+    assert.isString(process.stdout.write.firstCall.args[0]);
+    assert.deepEqual(JSON.parse(process.stdout.write.firstCall.args[0]), {application: 'nice-microservice', logLevel: 30, '@timestamp': timestamp, message: 'event happened', sessionId: '123'});    
+  });
+
+  it('should write message encoded with defined layout', () => {
+    //given
+    const encoder = bunyanEncoder({time: 'customTime', msg: 'customMsg'});
     const timestamp = new Date().toISOString();
     //when
     encoder.write({v: '1', time: timestamp, msg: 'event happened', sessionId: '123'});
     //then
-    assert.isTrue(stream.write.calledOnce);
-    assert.isString(stream.write.firstCall.args[0]);
-    assert.deepEqual(JSON.parse(stream.write.firstCall.args[0]), {'@timestamp': timestamp, message: 'event happened', sessionId: '123'});
+    assert.isTrue(process.stdout.write.calledOnce);
+    assert.isString(process.stdout.write.firstCall.args[0]);
+    assert.deepEqual(JSON.parse(process.stdout.write.firstCall.args[0]), {customTime: timestamp, customMsg: 'event happened', sessionId: '123'});
   });
 
-  it('should write to stdout stream if no stream specified', () => {
+  it('should write encoded message to a defined stream', () => {
     //given
-    sandbox.stub(process.stdout, 'write');
-    const encoder = bunyanEncoder({msg: 'message'});
+    const stream = {write: sinon.spy()};
+    const encoder = bunyanEncoder({msg: 'message'}, stream);
     //when
     encoder.write({msg: 'event happened'});
     //then
-    assert.isTrue(process.stdout.write.calledOnce);
+    assert.isTrue(stream.write.calledOnce);
   });
 });
